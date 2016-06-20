14b6d1e0ae6a37cbbb88161aa74c1742cdc61c54
Tyler Hensley <Tyler Hensley>
2016-06-20 17:33:06 -0400

    Added getters/setters and a method for Tender received and change to be returned

--- src/ForCash.java
+++ src/ForCash.java
@@ -1,4 +1,38 @@
 
 public class ForCash {
-	// ask for amount tendered and provide change 
+	private double change;
+	private double cashPaid;
+	
+	
+	
+	public ForCash(double price, double cashPaid){
+		
+		this.cashPaid = cashPaid;
+		this.change = (cashPaid - price);
+		
+		
+	}
+	
+	
+	public double getChange() {
+		return change;
+	}
+
+
+	public double getCashPaid() {
+		return cashPaid;
+	}
+
+
+	public void setChange(double change) {
+		this.change = change;
+	}
+
+
+	public void setCashPaid(double cashPaid) {
+		this.cashPaid = cashPaid;
+	}
+
+
+
 }
