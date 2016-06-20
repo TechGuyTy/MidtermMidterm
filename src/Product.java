95d6b8dad1a9392f0d45f22d30b3723e2395b2d4
AlexChen <alexchentaiwan@gmail.com>
2016-06-21 04:21:52 +0800

    modified product

--- src/Product.java
+++ src/Product.java
@@ -4,4 +4,48 @@ public class Product {
 	private String category; 
 	private String description; 
 	private double price; 
+	
+	//constructor 
+	public Product(String name, String category, String description, double price){
+		this.name = name;
+		this.category = category;
+		this.price = price;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public String toString() {
+		return name + "(" + category + ")" + description + price;
+	}
+	
 }
