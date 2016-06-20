cf966647a40ec303d3d3f28663399c699ee09f7e
Tyler Hensley <Tyler Hensley>
2016-06-20 17:57:35 -0400

    Added items to ArrayList

--- src/ProductReader.java
+++ src/ProductReader.java
@@ -1,12 +1,14 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 public class ProductReader {
 	public static void main(String[] args) {
-		
 
-	//public static void foodReader() {
+		// public static void foodReader() {
+
+		ArrayList<String> productList = new ArrayList<String>();
 
 		try {
 			// creating new file object
@@ -20,13 +22,25 @@ public class ProductReader {
 			// reading the file
 			String line = reader.readLine();
 
+			int i = 0;
+			String[] productArray = {};
+
 			while (line != null) {
-				
+
 				String[] details = line.split("\t");
-				System.out.println(line);
+				// System.out.println(line);
+
 				line = reader.readLine();
+
+				productList.add(line);
+				
+				System.out.println(line);
+				
+
 			}
-			
+
+			System.out.println("Here's an attempt at an array" + productList.get(3) );
+
 			reader.close();
 
 		} catch (Exception ex) {
@@ -34,6 +48,5 @@ public class ProductReader {
 			ex.printStackTrace();
 		}
 	}
-
 
 }
