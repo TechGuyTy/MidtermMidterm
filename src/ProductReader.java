4fdd542def33ce1f89b96a310d6f768006f63053
Steve Goldman <gutezach@gmail.com>
2016-06-20 16:16:02 -0400

    adding file reader

---
+++ src/ProductReader.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class ProductReader {
+	
+	public static void foodReader() {
+
+		try {
+			// creating new file object
+			File reader = new File("Products.txt");
+			// creating filereader connection
+			FileReader fileReader = new FileReader(reader);
+
+			// creating reader chain
+			BufferedReader reader = new BufferedReader(fileReader);
+
+			// reading the file
+			String line = reader.readLine();
+
+			while (line != null) {
+				
+				String[] details = line.split("\t");
+				System.out.println(line);
+				line = reader.readLine();
+			}
+			
+			reader.close();
+
+		} catch (Exception ex) {
+			// TODO Auto-generated catch block
+			ex.printStackTrace();
+		}
+	}
+
+
+}
