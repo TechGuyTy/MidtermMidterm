349064fcc3735bca59f88742ddb09e5467ced297
Steve Goldman <gutezach@gmail.com>
2016-06-20 19:59:41 -0400

    added responsive menu linked to array list

---
+++ src/ChooseMenu.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ChooseMenu {
+	
+	public static int getChoice(Scanner sc, String prompt) {
+		
+		System.out.println(prompt);
+		int choice = sc.nextInt();
+		
+		switch (choice) {
+		case 1: 
+			
+			break;
+
+		default:
+			break;
+		}
+		
+		
+		return 0;
+	}
+	
+	
+}
